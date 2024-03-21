dfbc356c2c958b76e923e968e68bb15776c4daea
AN78AA <164301765+AN78AA@users.noreply.github.com>
2024-03-21 01:21:16 -0600

    Update d.c

--- d.c
+++ d.c
@@ -14,11 +14,8 @@ double *sums;
 double x = 1.0;
 
 int *proc_count;
-int *start_all;
+sem_t *start_semaphore;
 double *res;
-
-sem_t *sem_proc_count;
-sem_t *sem_start_all;
 
 double get_member(int n, double x) {
     int i;
@@ -35,28 +32,26 @@ double get_member(int n, double x) {
 
 void proc(int proc_num) {
     int i;
-
-    sem_wait(sem_start_all); // Esperar hasta que se inicie el cálculo
-
+    sem_wait(start_semaphore); // Espera hasta que el semáforo se active
     sums[proc_num] = 0;
     for (i = proc_num; i < SERIES_MEMBER_COUNT; i += NPROCS)
         sums[proc_num] += get_member(i + 1, x);
 
-    sem_post(sem_proc_count); // Indicar que el proceso ha terminado su cálculo
+    sem_post(start_semaphore); // Incrementa el semáforo para indicar que ha terminado
     exit(0);
 }
 
 void master_proc() {
     int i;
 
-    sleep(1);
-    *start_all = 1;
-    sem_post(sem_start_all); // Indicar que los cálculos pueden comenzar
+    sleep(1); // Damos un pequeño tiempo para que los procesos esclavos se preparen
+    sem_post(start_semaphore); // Activamos el semáforo para que los procesos esclavos comiencen
 
     for (i = 0; i < NPROCS; i++)
-        sem_wait(sem_proc_count); // Esperar a que todos los procesos hayan terminado
+        sem_wait(start_semaphore); // Esperamos a que todos los procesos esclavos terminen
 
     *res = 0;
+
     for (i = 0; i < NPROCS; i++)
         *res += sums[i];
 
@@ -64,57 +59,56 @@ void master_proc() {
 }
 
 int main() {
+    int *threadIdPtr;
+
+    long long start_ts;
+    long long stop_ts;
+    long long elapsed_time;
+    long lElapsedTime;
+    struct timeval ts;
     int i;
+    int p;
     int shmid;
     void *shmstart;
 
-    // Inicialización de semáforos
-    sem_proc_count = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
-    sem_start_all = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
-
-    sem_init(sem_proc_count, 1, 0); // Inicialmente, no hay procesos completados
-    sem_init(sem_start_all, 1, 0); // Inicialmente, no se ha iniciado el cálculo
-
-    // Creación de la memoria compartida
-    shmid = shmget(IPC_PRIVATE, (NPROCS * sizeof(double)) + (2 * sizeof(int)), 0666 | IPC_CREAT);
+    shmid = shmget(0x1234, NPROCS * sizeof(double) + 2 * sizeof(int), 0666 | IPC_CREAT);
     shmstart = shmat(shmid, NULL, 0);
-    sums = (double*)shmstart;
-    proc_count = (int*)(shmstart + (NPROCS * sizeof(double)));
-    start_all = (int*)(shmstart + (NPROCS * sizeof(double)) + sizeof(int));
-    res = (double*)(shmstart + (NPROCS * sizeof(double)) + (2 * sizeof(int)));
+    sums = shmstart;
+    proc_count = shmstart + NPROCS * sizeof(double);
+    start_semaphore = (sem_t*)(shmstart + NPROCS * sizeof(double) + sizeof(int));
+    res = shmstart + NPROCS * sizeof(double) + 2 * sizeof(int);
+
+    sem_init(start_semaphore, 1, 0); // Inicializa el semáforo con 0
 
     *proc_count = 0;
-    *start_all = 0;
 
-    // Creación de los procesos
+    gettimeofday(&ts, NULL);
+    start_ts = ts.tv_sec; // Tiempo inicial
+
     for (i = 0; i < NPROCS; i++) {
-        if (fork() == 0) {
+        p = fork();
+        if (p == 0)
             proc(i);
-            exit(0);
-        }
     }
 
-    // Creación del proceso maestro
-    if (fork() == 0) {
+    p = fork();
+    if (p == 0)
         master_proc();
-        exit(0);
-    }
 
-    // Esperar a que todos los procesos terminen
-    for (i = 0; i < NPROCS + 1; i++)
-        wait(NULL);
-
-    // Impresión del resultado
     printf("El recuento de ln(1 + x) miembros de la serie de Mercator es %d\n", SERIES_MEMBER_COUNT);
     printf("El valor del argumento x es %f\n", (double)x);
+
+    for (int i = 0; i < NPROCS + 1; i++)
+        wait(NULL);
+
+    gettimeofday(&ts, NULL);
+    stop_ts = ts.tv_sec; // Tiempo final
+    elapsed_time = stop_ts - start_ts;
+    printf("Tiempo = %lld segundos\n", elapsed_time);
     printf("El resultado es %10.8f\n", *res);
     printf("Llamando a la función ln(1 + %f) = %10.8f\n", x, log(1 + x));
 
-    // Liberación de recursos
+    sem_destroy(start_semaphore); // Destruye el semáforo
     shmdt(shmstart);
     shmctl(shmid, IPC_RMID, NULL);
-    sem_destroy(sem_proc_count);
-    sem_destroy(sem_start_all);
-
-    return 0;
 }
